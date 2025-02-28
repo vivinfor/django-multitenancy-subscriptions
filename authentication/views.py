c2853fe090717921050d4595faa2e2f1ef679970
vivinfor <133239923+vivinfor@users.noreply.github.com>
2025-02-28 20:31:28 -0300

    update authentication.views

--- authentication/views.py
+++ authentication/views.py
@@ -8,24 +8,6 @@ from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib import messages
 
-from .models import CustomUser
-
-class UserProfileView(LoginRequiredMixin, UpdateView):
-    """
-    View para exibir e editar o perfil do usuário logado.
-    """
-    model = CustomUser
-    form_class = ProfileForm
-    template_name = "authentication/profile.html"
-    success_url = reverse_lazy("authentication:profile")
-
-    def get_object(self, queryset=None):
-        return self.request.user
-
-    def form_valid(self, form):
-        messages.success(self.request, "Seu perfil foi atualizado com sucesso!")
-        return super().form_valid(form)
-
 @method_decorator(csrf_exempt, name='dispatch')
 class CustomLoginView(LoginView):
     """
