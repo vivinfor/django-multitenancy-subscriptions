2c93c282b649904f5f59defa1cf0a3e91c963250
vivinfor <133239923+vivinfor@users.noreply.github.com>
2025-02-28 20:36:26 -0300

    add config templates into app.settings

--- app/settings.py
+++ app/settings.py
@@ -111,6 +111,27 @@ REST_FRAMEWORK = {
 }
 
 # ====================================================
+# Configurações dos Templates Django
+# ====================================================
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+
+# ====================================================
 # Configurações de JWT
 # ====================================================
 SIMPLE_JWT = {
