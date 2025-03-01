2f866953a5bbeadadfb6a2eef243da1ecaebfdab
vivinfor <133239923+vivinfor@users.noreply.github.com>
2025-02-28 21:20:04 -0300

    add static folder

--- app/settings.py
+++ app/settings.py
@@ -45,11 +45,9 @@ INSTALLED_APPS = [
 # ====================================================
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
-
+# Diretório onde os arquivos estáticos serão coletados
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # ====================================================
